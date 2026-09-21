e8d9378497bbc5112a9b17bad3ee0ef78ed41913
temoks206 <185912465+temoks206@users.noreply.github.com>
2026-09-21 19:20:24 +0000

    Добавил статус игр в GameSession и таблицу для фиксирования выстрелов

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, JSON
+from sqlalchemy import String, JSON, ForeignKey, CheckConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import UUID
 
@@ -20,6 +20,13 @@ class User(Base):
 class GameSession(Base):
     __tablename__ = "game_sessions"
 
+    __table_args__ = (
+        CheckConstraint(
+            "status IN ('opened', 'closed')",
+            name="ck_game_sessions_status",
+        ),
+    )
+
     id: Mapped[int] = mapped_column(primary_key=True)
 
     session_id: Mapped[uuid.UUID] = mapped_column(
@@ -33,3 +40,48 @@ class GameSession(Base):
         JSON,
         nullable=False,
     )
+
+    status: Mapped[str] = mapped_column(
+        String(10),
+        nullable=False,
+        default="opened",
+    )
+
+
+
+
+class Shot(Base):
+    __tablename__ = "shots"
+
+    __table_args__ = (
+        CheckConstraint(
+            "side IN ('self', 'opponent')",
+            name="ck_shots_side",
+        ),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    game_session_id: Mapped[int] = mapped_column(
+        ForeignKey("game_sessions.id"),
+        nullable=False,
+    )
+
+    side: Mapped[str] = mapped_column(
+        String(10),
+        nullable=False,
+    )
+
+    row: Mapped[int] = mapped_column(
+        nullable=False,
+    )
+
+    column: Mapped[int] = mapped_column(
+        nullable=False,
+    )
+
+    result: Mapped[str | None] = mapped_column(
+        String(10),
+        nullable=True,
+    )
+
